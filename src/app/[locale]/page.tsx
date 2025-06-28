e3f704e2d445b26e92c9b2baddfe920ec84b4c44
LaryDev <laryenockh@gmail.com>
2025-06-28 21:44:34 +0100

    portfolio v1

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,31 +1,21 @@
-import { notFound } from 'next/navigation';
-import Hero from '../../components/Hero';
-import About from '../../components/About';
-import Projects from '../../components/Projects';
-import Contact from '../../components/Contact';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
+import Projects from '@/components/Projects';
+import Contact from '@/components/Contact';
 import Header from '@/components/Header';
 import Skills from '@/components/Skills';
 import Footer from '@/components/Footer';
 
-const locales = ['en', 'fr'];
-
-export default async function Home(
-    { params }: { params: { locale: string } }
-
-) {
-    const locale = params.locale;
-    if (!locales.includes(locale)) {
-        notFound();
-    }
-    return (
-        <>
-            <Header />
-            <Hero />
-            <About />
-            <Skills />
-            <Projects />
-            <Contact />
-            <Footer />
-        </>
-    );
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
