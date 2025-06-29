cfc75f408921eb4342b114a9bf9615db113c5ab3
LaryDev <laryenockh@gmail.com>
2025-06-29 15:26:13 +0100

    portfolio v1

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,20 +1,22 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 
-export default async function LocaleLayout(props: {
-    children: React.ReactNode;
-    params: { locale: string };
+export default async function LocaleLayout({
+  children,
+  params: { locale }
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
 }) {
-    const messages = await getMessages();
-    const locale = await Promise.resolve(props.params.locale);
+  const messages = await getMessages({ locale });
 
-    return (
-        <html lang={locale}>
-            <body>
-                <NextIntlClientProvider messages={messages}>
-                    {props.children}
-                </NextIntlClientProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
